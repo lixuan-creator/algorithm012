a272480babf7137178cf3b72a620068086b6c781
lixuan-creator <1104181066@qq.com>
2020-07-12 12:23:32 +0800

    第一次提交作业：重新梳理代码及总结

--- Week_01/MoveZeroes.java
+++ Week_01/MoveZeroes.java
@@ -1,57 +1,24 @@
 package com.algorithm.week01;
 
-/**
- * 移动零
- * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
- * 输入: [0,1,0,3,12]
- * 输出: [1,3,12,0,0]
- * https://leetcode-cn.com/problems/move-zeroes/
- */
+
 public class MoveZeroes {
-    /**
-     * 简单易懂
-     * 这里参考了快速排序的思想，快速排序首先要确定一个待分割的元素做中间点x，然后把所有小于等于x的元素放到x的左边，大于x的元素放到其右边。
-     * 这里我们可以用0当做这个中间点，把不等于0(注意题目没说不能有负数)的放到中间点的左边，等于0的放到其右边。
-     * 这的中间点就是0本身，所以实现起来比快速排序简单很多，我们使用两个指针i和j，只要nums[i]!=0，我们就交换nums[i]和nums[j]
-     * 链接：https://leetcode-cn.com/problems/move-zeroes/solution/dong-hua-yan-shi-283yi-dong-ling-by-wang_ni_ma/
-     * 时间复杂度:O(n)
-     * 空间复杂度:O(1)
-     */
-    public static void moveZeroes(int[] nums) {
+
+    public void moveZeroes(int[] nums) {
         if (nums == null) {
             return ;
         }
-        //第一次遍历的时候，j指针记录非0的个数，只要是非0的统统都赋给nums[j]
         int j = 0;
         for (int i=0;i<nums.length;++i) {
             if(nums[i]!=0) {
                 nums[j++] = nums[i];
             }
         }
-        //非0元素统计完了，剩下的都是0了
-        //所以第二次遍历把末尾的元素都赋为0即可
         for (int i=j;i<nums.length;++i) {
             nums[i] = 0;
         }
     }
 
-
-    /**
-     * 经典
-     * 没怎么接触算法，双下标思路很神奇
-     * 自己写两个数组，也能写，不巧
-     * 有些东西自己确实写不出来，看别人的，学会了，也能带来很大提升
-     * 思路清楚了，默写还是很简单的
-     *
-     * 定义一个非零的指针 j=0，循环遍历数组用指针j记录非零值nums[j] = num[i];
-     * 判断两个指针i与j是否相等，这里两个指针初始值都为0，j指针只有在非零的情况下会j++；
-     * 因此可以判断j指针对应的值都为非零的数据，且保存原有顺序；
-     * 所以，原有数组继续遍历情况下，nums[i]都为0。
-     * 链接：https://leetcode-cn.com/problems/move-zeroes/solution/yi-dong-ling-by-yijiaoqian/
-     * 时间复杂度:O(n)
-     * 空间复杂度:O(1)
-     */
-    public static void moveZeroes2(int[] nums){
+    public void moveZeroes2(int[] nums){
         int j = 0;
         for (int i = 0; i< nums.length; i++) {
             if (nums[i] != 0) {
--- Week_01/TwoSum.java
+++ Week_01/TwoSum.java
@@ -1,39 +1,11 @@
 package com.algorithm.week01;
 
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 两数求和
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- * 给定 nums = [2, 7, 11, 15], target = 9
- * 所以返回 [0, 1]
- * https://leetcode-cn.com/problems/two-sum/
- */
 public class TwoSum {
 
-    @Test
-    public void twoSumTest() {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 13;
-
-        int[] ret = twoSum(nums, target);
-        for (int i : ret) {
-            System.out.println(i);
-        }
-    }
-
-    /**
-     * 经典
-     * 利用哈希表存储的方法很巧妙，同时将两数和转化为比较两数的值，思路确实开阔
-     * 链接：https://leetcode-cn.com/problems/two-sum/solution/jie-suan-fa-1-liang-shu-zhi-he-by-guanpengchn/
-     * 时间复杂度：O(n),只遍历了包含有 n 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
-     * 空间复杂度：O(n),所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 n 个元素。
-     */
-    public int[] twoSum(int[] nums, int target) {
+    public int[] towSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i< nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
@@ -43,6 +15,5 @@ public class TwoSum {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
-
 
 }
